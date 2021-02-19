da8b88697925dc5ed5c615fbf256a2ce6dee0b43
Adam <adam@hivemapper.com>
2021-02-19 23:13:47 +0000

    More batching fixes

--- packnet_sfm/geometry/camera_generic.py
+++ packnet_sfm/geometry/camera_generic.py
@@ -11,8 +11,6 @@ from packnet_sfm.geometry.camera_utils import scale_intrinsics
 from packnet_sfm.utils.image import image_grid
 import torch.nn.functional as F
 
-import pdb
-
 ########################################################################################################################
 
 
@@ -35,7 +33,6 @@ class GenericCamera(nn.Module):
         """
         super().__init__()
         self.ray_surface = R
-        # self.Tcw = Pose.identity(1) if Tcw is None else Tcw
         self.Tcw = Pose.identity(R.shape[0]) if Tcw is None else Tcw
 
     def to(self, *args, **kwargs):
@@ -74,9 +71,7 @@ class GenericCamera(nn.Module):
         B, C, H, W = depth.shape
         assert C == 1
 
-        # pdb.set_trace()
-
-        Xc = self.ray_surface * depth[0].unsqueeze(0)
+        Xc = self.ray_surface * depth
 
         # If in camera frame of reference
         if frame == 'c':
@@ -156,7 +151,6 @@ class GenericCamera(nn.Module):
 
         def _get_value_coords(x, coords):
 
-            # pdb.set_trace()
             b, c, h, w = x.shape
             n, k, _ = coords.shape  #[H*W, K, 2]
             coords = coords.reshape(-1, 2) #[H*W*K, 2]
@@ -185,43 +179,6 @@ class GenericCamera(nn.Module):
         direction_norm = direction_norm / \
             torch.norm(direction_norm, dim=1, keepdim=True)
 
-        #######################################################
-        # Old and busted
-        #######################################################
-
-        # direction_view = direction_norm.view(B, 3, H*W).squeeze()
-
-        # patch_ray_view = ray_surf_patch.squeeze().view(3, H*W, K)
-
-        # ray_logits = torch.bmm(direction_view.permute(1, 0).unsqueeze(
-        #     1), patch_ray_view.permute(1, 0, 2)).squeeze()
-
-        # # Softmax with temperature
-        # temperature = np.maximum(
-        #     min_temp, start_temp/np.exp(constant * progress))
-        # image_coords_softmax = torch.softmax(ray_logits / temperature, -1)
-
-        # image_coords = torch.bmm(
-        #     image_coords_softmax.unsqueeze(1), patch_coordinates.float())
-        # image_coords = image_coords.squeeze().view(H, W, 2)
-
-        # Xnorm = 2 * image_coords[:, :, 0] / (H - 1) - 1.
-        # Ynorm = 2 * image_coords[:, :, 1] / (W - 1) - 1.
-
-        # if downsample:
-        #     Xnorm = F.interpolate(Xnorm.unsqueeze(0).unsqueeze(
-        #         0), mode='bilinear', scale_factor=2.0, align_corners=True).squeeze()
-        #     Ynorm = F.interpolate(Ynorm.unsqueeze(0).unsqueeze(
-        #         0), mode='bilinear', scale_factor=2.0, align_corners=True).squeeze()
-        #     H = H * 2
-        #     W = W * 2
-
-        # return torch.stack([Ynorm, Xnorm], dim=-1).view(B, H, W, 2)
-
-        #######################################################
-        # New hotness
-        #######################################################
-
         projections = []
         for b in range(B):
             direction_view = direction_norm[b,:,:].view(1, 3, H*W).squeeze()
@@ -242,7 +199,6 @@ class GenericCamera(nn.Module):
             Xnorm = 2 * image_coords[:, :, 0] / (H - 1) - 1.
             Ynorm = 2 * image_coords[:, :, 1] / (W - 1) - 1.
 
-            # pdb.set_trace()
             if downsample:
                 Xnorm = F.interpolate(Xnorm.unsqueeze(0).unsqueeze(
                     0), mode='bilinear', scale_factor=2.0, align_corners=True).squeeze()
@@ -256,5 +212,4 @@ class GenericCamera(nn.Module):
             H = H * 2
             W = W * 2
 
-        # pdb.set_trace()
         return torch.stack(projections).view(B, H, W, 2)
