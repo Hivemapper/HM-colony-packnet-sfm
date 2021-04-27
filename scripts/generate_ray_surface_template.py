Fix norm calculation

scripts/generate_ray_surface_template.py
@@ -12,6 +12,10 @@ parser.add_argument("--h", type=int, help="Template height")
 parser.add_argument("--w", type=int, help="Template width")
 parser.add_argument("--o", type=str, help="Output file name without extension")
 parser.add_argument("--crop", type=int, help="Optional. Crop template after initialization")
+parser.add_argument("--normType",  
+                    type=str,   
+                    choices=['packnet', 'l2'],  
+                    help="Packnet: z=1 (ie divide by 'z' component of each vector). OR l2 norm for each vector")
 args = parser.parse_args()
 
 w = args.w
@@ -33,7 +37,12 @@ for v in range(h):                  #row
 p = np.stack(p)         #p.shape = (w*h, 3)
 Q = Kinv @ p.T          #Q.shape = (3, w*h)
 
-Q_norm = normalize(Q, axis=0)   #Q.shape = (3, w*h) 
+if args.normType == 'l2
+    Q_norm = normalize(Q, axis=0)   #Q.shape = (3, w*h) 
+elif args.normType == 'packnet':
+    Q_norm = Q / Q[2, :]
+else:
+    sys.exit('Wrong normalization type')
 
 Q_ray_surface = np.zeros((1,3,h,w))
 for v in range(w*h):